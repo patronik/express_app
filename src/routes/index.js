43c67e2731f2ee25556acdfdf8eab55df7be8dff
Vasyl Voina <vasyl.voina@gmail.com>
2024-10-29 22:49:44 +0200

    Add contacts

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,28 @@ const router = express.Router();
 // Home route
 router.get('/', (req, res) => {
   res.render('index', {       
-      title: 'Home page' 
+      title: 'Voinatech | Vasyl\'s Voina Personal Website | Home' 
+    }
+  );
+});
+
+router.get('/about', (req, res) => {
+  res.render('about', {       
+      title: 'Voinatech | Vasyl\'s Voina Personal Website | About' 
+    }
+  );
+});
+
+router.get('/projects', (req, res) => {
+  res.render('projects', {       
+      title: 'Voinatech | Vasyl\'s Voina Personal Website | Projects' 
+    }
+  );
+});
+
+router.get('/contact', (req, res) => {
+  res.render('contact', {       
+      title: 'Voinatech | Vasyl\'s Voina Personal Website | Contact' 
     }
   );
 });
