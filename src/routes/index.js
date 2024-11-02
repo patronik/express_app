1e0211b76d1f87a21d135e8056b968e7b474b0a0
Vasyl Voina <vasyl.voina@gmail.com>
2024-11-02 12:36:36 +0200

    Add CV download button

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import path  from 'path';
+import { fileURLToPath } from 'url';
 
 const router = express.Router();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Home route
 router.get('/', (req, res) => {
@@ -15,6 +18,18 @@ router.get('/about', (req, res) => {
       title: 'Voinatech | Vasyl\'s Voina Personal Website | About' 
     }
   );
+});
+
+router.get('/cv', (req, res) => {
+  const filePath = path.join(__dirname, '../../cv.pdf'); 
+
+  // Set headers to prompt download
+  res.download(filePath, 'CV_Vasyl_Voina.pdf', (err) => {
+      if (err) {
+          console.error("File download error:", err);
+          res.status(500).send("Error downloading file.");
+      }
+  });
 });
 
 router.get('/projects', (req, res) => {
